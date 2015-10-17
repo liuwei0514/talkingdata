eefc8f72d52cd123affb7d72679fdee8aa639041
leslie <liuweinot@163.com>
2015-10-18 00:40:20 +0800

    解决不加载TalkingDate.js的问题

--- www/TalkingData.js
+++ www/TalkingData.js
@@ -1,42 +1,51 @@
-var TalkingData = {
-    init:function(appKey, channelId) {
-        Cordova.exec(null, null, "TalkingData", "sessionStarted", [appKey, channelId]);
-    },
-    trackEvent:function(eventId) {
-        Cordova.exec(null, null, "TalkingData", "trackEvent", [eventId]);
-    },
-    trackEventWithLabel:function(eventId, eventLabel) {
-        Cordova.exec(null, null, "TalkingData", "trackEventWithLabel", [eventId, eventLabel]);
-    },
-    trackEventWithParameters:function(eventId, eventLabel, eventData) {
-        var eventDataJson = JSON.stringify(eventData);
-        Cordova.exec(null, null, "TalkingData", "trackEventWithParameters", [eventId, eventLabel, eventDataJson]);
-    },
-    trackPage:function(pageName) {
-        Cordova.exec(null, null, "TalkingData", "trackPage", [pageName]);
-    },
-    trackPageBegin:function(pageName) {
-        Cordova.exec(null, null, "TalkingData", "trackPageBegin", [pageName]);
-    },
-    trackPageEnd:function(pageName) {
-        Cordova.exec(null, null, "TalkingData", "trackPageEnd", [pageName]);
-    },
-    setLocation:function(latitude, longitude) {
-        Cordova.exec(null, null, "TalkingData", "setLocation", [latitude, longitude]);
-    },
-    getDeviceId:function(callBack) {
-        Cordova.exec(callBack, null, "TalkingData", "getDeviceId", []);
-    },
-    openDebugLog:function(enabled) {
-        Cordova.exec(null, null, "TalkingData", "openDebugLog", [enabled]);
-    },
-    setExceptionReportEnabled:function(enabled) {
-        Cordova.exec(null, null, "TalkingData", "setExceptionReportEnabled", [enabled]);
-    },
-    setSignalReportEnabled:function(enabled) {
-        Cordova.exec(null, null, "TalkingData", "setSignalReportEnabled", [enabled]);
-    },
-    setLogEnabled:function(enabled) {
-        Cordova.exec(null, null, "TalkingData", "setLogEnabled", [enabled]);
-    }
-};
+var TalkingData = function() {};
+TalkingData.prototype.init = function(appKey, channelId) {
+    Cordova.exec(null, null, "TalkingData", "sessionStarted", [appKey, channelId]);
+}
+TalkingData.prototype.trackEvent = function(eventId) {
+    Cordova.exec(null, null, "TalkingData", "trackEvent", [eventId]);
+}
+TalkingData.prototype.trackEventWithLabel = function(eventId, eventLabel) {
+    Cordova.exec(null, null, "TalkingData", "trackEventWithLabel", [eventId, eventLabel]);
+}
+TalkingData.prototype.trackEventWithParameters = function(eventId, eventLabel, eventData) {
+    var eventDataJson = JSON.stringify(eventData);
+    Cordova.exec(null, null, "TalkingData", "trackEventWithParameters", [eventId, eventLabel, eventDataJson]);
+}
+TalkingData.prototype.trackPage = function(pageName) {
+    Cordova.exec(null, null, "TalkingData", "trackPage", [pageName]);
+}
+TalkingData.prototype.trackPageBegin = function(pageName) {
+    Cordova.exec(null, null, "TalkingData", "trackPageBegin", [pageName]);
+}
+TalkingData.prototype.trackPageEnd = function(pageName) {
+    Cordova.exec(null, null, "TalkingData", "trackPageEnd", [pageName]);
+}
+TalkingData.prototype.setLocation = function(latitude, longitude) {
+    Cordova.exec(null, null, "TalkingData", "setLocation", [latitude, longitude]);
+}
+TalkingData.prototype.getDeviceId = function(callBack) {
+    Cordova.exec(callBack, null, "TalkingData", "getDeviceId", []);
+}
+TalkingData.prototype.openDebugLog = function(enabled) {
+    Cordova.exec(null, null, "TalkingData", "openDebugLog", [enabled]);
+}
+TalkingData.prototype.setExceptionReportEnabled = function(enabled) {
+    Cordova.exec(null, null, "TalkingData", "setExceptionReportEnabled", [enabled]);
+}
+TalkingData.prototype.setSignalReportEnabled = function(enabled) {
+    Cordova.exec(null, null, "TalkingData", "setSignalReportEnabled", [enabled]);
+}
+TalkingData.prototype.setLogEnabled = function(enabled) {
+    Cordova.exec(null, null, "TalkingData", "setLogEnabled", [enabled]);
+}
+
+if (!window.plugins) {
+    window.plugins = {};
+}
+
+if (!window.plugins.TalkingData) {
+    window.plugins.TalkingData = new TalkingData();
+}
+
+module.exports = new TalkingData();
